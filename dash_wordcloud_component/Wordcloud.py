26d537e41a22a97954eb6e4f8c6857d8bb9b5345
Ellison <EVOFORGE\lnelli2@a-3l03vk6nesiqe.evoforge.org>
2019-07-11 13:05:47 -1000

    working wordcloud

--- dash_wordcloud_component/Wordcloud.py
+++ dash_wordcloud_component/Wordcloud.py
@@ -13,15 +13,22 @@ which is editable by the user.
 
 Keyword arguments:
 - id (string; optional): The ID used to identify this component in Dash callbacks
-- label (string; required): A label that will be printed when this component is rendered.
-- value (string; optional): The value displayed in the input"""
+- className (string; optional)
+- options (list; optional)
+- text (string; optional)
+- style (dict; optional)
+- loading_state (optional): . loading_state has the following type: dict containing keys 'is_loading', 'prop_name', 'component_name'.
+Those keys have the following types:
+  - is_loading (boolean; optional)
+  - prop_name (string; optional)
+  - component_name (string; optional)"""
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, label=Component.REQUIRED, value=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'label', 'value']
+    def __init__(self, id=Component.UNDEFINED, className=Component.UNDEFINED, options=Component.UNDEFINED, text=Component.UNDEFINED, style=Component.UNDEFINED, loading_state=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'className', 'options', 'text', 'style', 'loading_state']
         self._type = 'Wordcloud'
         self._namespace = 'dash_wordcloud_component'
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'label', 'value']
+        self.available_properties = ['id', 'className', 'options', 'text', 'style', 'loading_state']
         self.available_wildcard_properties =            []
 
         _explicit_args = kwargs.pop('_explicit_args')
@@ -29,7 +36,7 @@ Keyword arguments:
         _locals.update(kwargs)  # For wildcard attrs
         args = {k: _locals[k] for k in _explicit_args if k != 'children'}
 
-        for k in ['label']:
+        for k in []:
             if k not in args:
                 raise TypeError(
                     'Required argument `' + k + '` was not specified.')
--- src/lib/components/Wordcloud.react.js
+++ src/lib/components/Wordcloud.react.js
@@ -9,53 +9,242 @@ import PropTypes from 'prop-types';
  * which is editable by the user.
  */
 export default class Wordcloud extends Component {
-    render() {
-        const {id, label, setProps, value} = this.props;
+  render() {
+      const {
+        id,
+        className,
+        options,
+        setProps,
+        loading_state,
+        text,
+        style
+      } = this.props;
+      // alert("rendering");
+      return (
+          <div
+            id={id}
+            data-dash-is-loading={(loading_state && loading_state.is_loading) || undefined}
+            className={className}
+            style={style}>
 
-        return (
-            <div id={id}>
-                ExampleComponent: {label}&nbsp;
-                <input
-                    value={value}
-                    onChange={
-                        /*
-                         * Send the new value to the parent component.
-                         * setProps is a prop that is automatically supplied
-                         * by dash's front-end ("dash-renderer").
-                         * In a Dash app, this will update the component's
-                         * props and send the data back to the Python Dash
-                         * app server if a callback uses the modified prop as
-                         * Input or State.
-                         */
-                        e => setProps({ value: e.target.value })
-                    }
-                />
-            </div>
-        );
+          </div>
+      );
+  }
+
+  componentDidMount() {
+    // alert("drawing");
+    const {
+      id,
+      className,
+      options,
+      setProps,
+      loading_state,
+      text,
+      style
+    } = this.props;
+
+    var i = 1;
+    var wordCount=0;
+    var colors = ["#33638D", "#21908D", "#A0DA39", "#4CC26C", "#D2E21B", "#450559", "#46337F", "#3F4889"];
+    var config = {
+        trace: true,
+        shapeResolution: 1, //Lower = better resolution
+        trialNumber: 360 * 5,
+        lineHeight: 0.8,
+        xWordPadding: .5,
+        yWordPadding: .5,
+        font: "sans-serif"
     }
+    var width = style.width;
+    var height = style.height;
+    width = width.substring(0, width.length - 2);
+    height = height.substring(0, height.length - 2);
+
+    // alert(id);
+    function getFreqs() {
+      // var text = this.props.text;
+      var words = text.split(/\s/);
+      var freqMap = [];
+      var wordCount=0;
+      words.forEach(function(w) {
+          if (!freqMap[w]) {
+              freqMap[w] = 0;
+          }
+          freqMap[w] += 1;
+          wordCount += 1;
+      })
+
+      var array = [];
+      for (var w in freqMap) {
+        array.push({
+          word: w,
+          freq: freqMap[w],
+          relFreq: freqMap[w]/wordCount
+        });
+      }
+      array.sort((a, b) => (b.freq > a.freq) ? 1 : -1)
+      return array;
+    }
+
+    var words = getFreqs();
+    console.log(words);
+    var cloud = document.getElementById(id);
+    cloud.style.position = "relative";
+    cloud.style.fontFamily = config.font;
+
+    var traceCanvas = document.createElement("canvas");
+    traceCanvas.width = cloud.offsetWidth;
+    traceCanvas.height = cloud.offsetHeight;
+    var traceCanvasCtx = traceCanvas.getContext("2d");
+    cloud.appendChild(traceCanvas);
+
+    var startPoint = {
+        x: cloud.offsetWidth / 2,
+        y: cloud.offsetHeight / 2
+    };
+
+    var wordsDown = [];
+    // var colorArray = options.colors;
+    var n = colors.length;
+
+    function createWordObject(word, freq, relFreq) {
+
+        var wordContainer = document.createElement("div");
+        wordContainer.style.position = "absolute";
+        wordContainer.style.fontSize = relFreq*width + "px";
+        wordContainer.style.color = colors[i%n];
+        wordContainer.style.lineHeight = config.lineHeight;
+        wordContainer.className = "tooltip";
+    /*    wordContainer.style.transform = "translateX(-50%) translateY(-50%)";*/
+        wordContainer.appendChild(document.createTextNode(word));
+        var tooltip = document.createElement("span");
+        tooltip.className = "tooltiptext";
+        tooltip.style.fontSize = 12 + "px";
+        tooltip.appendChild(document.createTextNode(freq));
+        wordContainer.appendChild(tooltip);
+        i += 1;
+        return wordContainer;
+    }
+
+    function placeWord(word, x, y) {
+
+        cloud.appendChild(word);
+        word.style.left = x - word.offsetWidth/2 + "px";
+        word.style.top = y - word.offsetHeight/2 + "px";
+
+        wordsDown.push(word.getBoundingClientRect());
+    }
+
+    function trace(x, y) {
+    //     traceCanvasCtx.lineTo(x, y);
+    //     traceCanvasCtx.stroke();
+        traceCanvasCtx.fillRect(x, y, 1, 1);
+    }
+
+    function generatePointInShape() {
+
+        var angle = Math.random() * height;
+        var x = Math.random() * width;
+        var y = Math.random() * height;
+        return [x, y];
+    }
+
+    function intersect(word, x, y) {
+        cloud.appendChild(word);
+
+        word.style.left = x - word.offsetWidth/2 + "px";
+        word.style.top = y - word.offsetHeight/2 + "px";
+
+        var currentWord = word.getBoundingClientRect();
+
+        cloud.removeChild(word);
+
+        for(var i = 0; i < wordsDown.length; i+=1){
+            var comparisonWord = wordsDown[i];
+
+            if(outOfBounds(currentWord) || !(currentWord.right + config.xWordPadding < comparisonWord.left - config.xWordPadding ||
+               currentWord.left - config.xWordPadding > comparisonWord.right + config.xWordPadding ||
+               currentWord.bottom + config.yWordPadding < comparisonWord.top - config.yWordPadding ||
+               currentWord.top - config.yWordPadding > comparisonWord.bottom + config.yWordPadding )){
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function outOfBounds(currentWord) {
+        var canvas = cloud.getBoundingClientRect();
+        if (currentWord.right + config.xWordPadding > canvas.right || currentWord.bottom + config.yWordPadding > canvas.bottom ||
+            currentWord.left - config.xWordPadding < 0 || currentWord.top - config.yWordPadding < 0) {
+          return true;
+        }
+        return false;
+    }
+    /* =======================  END PLACEMENT FUNCTIONS =======================  */
+
+
+
+
+
+    /* =======================  LETS GO! =======================  */
+    (function placeWords() {
+        for (var i = 0; i < words.length; i += 1) {
+            var word = createWordObject(words[i].word, words[i].freq, words[i].relFreq);
+            if (i==0) {
+              placeWord(word, startPoint.x, startPoint.y);
+            }
+            else {
+              for (var j = 0; j < config.trialNumber; j++) {
+                  // alert(j);
+                  //If the spiral function returns true, we've placed the word down and can break from the j loop
+                  var point = generatePointInShape();
+                  var x = point[0];
+                  var y = point[1];
+                  if (!intersect(word, x, y)) {
+                    placeWord(word, x, y);
+                    // return true;
+                    break;
+                  }
+              }
+            }
+        }
+    })();
+  }
 }
 
-Wordcloud.defaultProps = {};
+Wordcloud.defaultProps = {
+options: [{hoverInfo: "", colors: ["#33638D", "#21908D", "#A0DA39", "#4CC26C", "#D2E21B", "#450559", "#46337F", "#3F4889"]}],
+};
 
 Wordcloud.propTypes = {
-    /**
-     * The ID used to identify this component in Dash callbacks
-     */
-    id: PropTypes.string,
+  /**
+   * The ID used to identify this component in Dash callbacks
+   */
+  id: PropTypes.string,
 
-    /**
-     * A label that will be printed when this component is rendered.
-     */
-    label: PropTypes.string.isRequired,
+  className: PropTypes.string,
 
-    /**
-     * The value displayed in the input
-     */
-    value: PropTypes.string,
+  options: PropTypes.arrayOf(
+    PropTypes.exact({
+      hoverInfo: PropTypes.string,
 
-    /**
-     * Dash-assigned callback that should be called whenever any of the
-     * properties change
-     */
-    setProps: PropTypes.func
+      colors: PropTypes.arrayOf(PropTypes.string)
+    })
+  ),
+
+  text: PropTypes.string,
+
+  style: PropTypes.object,
+
+  setProps: PropTypes.func,
+
+  loading_state: PropTypes.shape({
+    is_loading: PropTypes.bool,
+
+    prop_name: PropTypes.string,
+
+    component_name: PropTypes.string
+  })
 };
