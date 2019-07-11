3e111f93e158b8cea5823bfbec604214fb8a8d99
Ellison <EVOFORGE\lnelli2@a-3l03vk6nesiqe.evoforge.org>
2019-07-11 13:08:32 -1000

    css added

--- dash_wordcloud_component/__init__.py
+++ dash_wordcloud_component/__init__.py
@@ -39,7 +39,12 @@ _js_dist = [
     }
 ]
 
-_css_dist = []
+_css_dist = [
+    {
+        'relative_package_path': 'wordcloud.css',
+        'namespace': package_name
+    }
+]
 
 
 for _component in __all__:
